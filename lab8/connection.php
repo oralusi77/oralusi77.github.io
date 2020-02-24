334dd52b405e7dd053b5bb785756c4a6ebff23e5
1909915 <oralusi77@gmail.com>
2020-02-24 13:23:34 +0000

    php

---
+++ lab8/connection.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'csdm-webdev');
+define('DB_USERNAME', '1909915');
+define('DB_PASSWORD', '1909915');
+define('DB_DATABASE', 'db1909915_cmm007');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
